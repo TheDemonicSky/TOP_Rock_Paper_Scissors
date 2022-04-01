e8acc8b0852dc12bf76ad2afb96bc6fe9c8d91d0
Skylar Allen <skylara@protonmail.com>
2022-03-31 20:00:27 -0700

    Added Round Logic

--- index.js
+++ index.js
@@ -10,3 +10,44 @@ function computerPlay() {
 	}
 	return(computerChoice);
 }
+
+function playRound(playerSelection, computerSelection) {
+	switch(playerSelection, computerSelection) {
+		// All the playerSelection == rock cases
+		case playerSelection == 'rock', computerSelection == 'paper':
+			console.log('Computer Wins');
+			break;
+		case playerSelection == 'rock', computerSelection == 'scissors':
+			console.log('Player Wins');
+			break;
+		case playerSelection == 'rock', computerSelection == 'rock':
+			console.log('Draw');
+			break;
+
+		// All the playerSelection == paper cases
+		case playerSelection == 'paper', computerSelection == 'paper':
+			console.log('Draw');
+			break;
+		case playerSelection == 'paper', computerSelection == 'scissors':
+			console.log('Computer Wins');
+			break;
+		case playerSelection == 'paper', computerSelection == 'rock':
+			console.log('Player Wins');
+			break;
+
+		//All the playerSelection == scissors cases
+		case playerSelection == 'scissors', computerSelection == 'paper':
+			console.log('Player Wins');
+			break;
+		case playerSelection == 'scissors', computerSelection == 'scissors':
+			console.log('Draw');
+			break;
+		case playerSelection == 'scissors', computerSelection == 'rock':
+			console.log('Computer Wins');
+			break;
+	}
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
