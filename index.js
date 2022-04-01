895b5a641c241c0995059ea7c7783072e80ac4f3
Skylar Allen <skylara@protonmail.com>
2022-03-31 20:02:18 -0700

    Fixed round logic

--- index.js
+++ index.js
@@ -13,41 +13,41 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
 	switch(playerSelection, computerSelection) {
-		// All the playerSelection == rock cases
-		case playerSelection == 'rock', computerSelection == 'paper':
+		// All the playerSelection = rock cases
+		case playerSelection = 'rock', computerSelection = 'paper':
 			console.log('Computer Wins');
 			break;
-		case playerSelection == 'rock', computerSelection == 'scissors':
+		case playerSelection = 'rock', computerSelection = 'scissors':
 			console.log('Player Wins');
 			break;
-		case playerSelection == 'rock', computerSelection == 'rock':
+		case playerSelection = 'rock', computerSelection = 'rock':
 			console.log('Draw');
 			break;
 
-		// All the playerSelection == paper cases
-		case playerSelection == 'paper', computerSelection == 'paper':
+		// All the playerSelection = paper cases
+		case playerSelection = 'paper', computerSelection = 'paper':
 			console.log('Draw');
 			break;
-		case playerSelection == 'paper', computerSelection == 'scissors':
+		case playerSelection = 'paper', computerSelection = 'scissors':
 			console.log('Computer Wins');
 			break;
-		case playerSelection == 'paper', computerSelection == 'rock':
+		case playerSelection = 'paper', computerSelection = 'rock':
 			console.log('Player Wins');
 			break;
 
-		//All the playerSelection == scissors cases
-		case playerSelection == 'scissors', computerSelection == 'paper':
+		//All the playerSelection = scissors cases
+		case playerSelection = 'scissors', computerSelection = 'paper':
 			console.log('Player Wins');
 			break;
-		case playerSelection == 'scissors', computerSelection == 'scissors':
+		case playerSelection = 'scissors', computerSelection = 'scissors':
 			console.log('Draw');
 			break;
-		case playerSelection == 'scissors', computerSelection == 'rock':
+		case playerSelection = 'scissors', computerSelection = 'rock':
 			console.log('Computer Wins');
 			break;
 	}
 }
 
-const playerSelection = 'rock'
+const playerSelection = 'scissors'
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
